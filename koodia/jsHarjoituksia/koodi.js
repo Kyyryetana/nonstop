06983ab448c38baab02cc0ecf5511c204545fc46
nikoalander1986 <niko.alander@hotmail.com>
2021-12-02 15:42:04 +0200

    joulu siivousta

--- koodia/jsHarjoituksia/koodi.js
+++ koodia/jsHarjoituksia/koodi.js
@@ -1,4 +1,4 @@
-var a = 2;
+/*var a = 2;
 var b = 3;
 var c = 8;
 var taulukkomuuttuja = [34, 566, 71, 89];
@@ -33,5 +33,43 @@ function tehtavaNelja() {
 function tehtavaViisi() {
   console.log(a * 3600 + b * 60 + c);
 }
+*/
 //6
 //kesken
+//
+// tunnin muistiinpanoja alhaalla
+//switch - case
+/*switch (valinta) {
+  case 1:
+    console.log("valitsit numeron 1");
+    break;
+    case 1:
+      console.log("valitsit numeron 1");
+      break;
+  case "J":
+    console.log("valitsit jätkän");
+    break;
+  default:
+    console.log("valitsit jotain muuta");
+    break;
+}
+//
+switch (kortti) {
+  case "J":
+  case "Q":
+  case "K":
+    pisteet += 5;
+    break;
+  case "10":
+  case "A":
+    pisteet += 10;
+    break;
+}
+*/
+/*var summa = 0;
+for(var i = 0; i < 10; i++)
+{
+  summa += i;
+  //console.log(i);
+}
+console.log("Lukujen 1-10 summa on: " + summa);
