376926ff3997b7df5600db1db56902b677e2e6da
nikoalander1986 <niko.alander@hotmail.com>
2021-12-16 12:29:36 +0200

    ..

--- ensimmäisetHarjoitukset/omatProjektit/viikon2EnsimmaisetHarjoitukset/script.js
+++ ensimmäisetHarjoitukset/omatProjektit/viikon2EnsimmaisetHarjoitukset/script.js
@@ -2,4 +2,5 @@ function jarjestaLuvut() {
   let luku1 = parseInt(document.getElementById('eka1').value);
   let luku2 = parseInt(document.getElementById('toka1').value);
   let luku3 = parseInt(document.getElementById('kolmas1').value);
+  console.log('luku1');
 }
