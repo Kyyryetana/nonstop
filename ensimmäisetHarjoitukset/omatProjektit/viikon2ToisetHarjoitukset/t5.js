871869c02efe7af528b55ae798e76f2d66ae0681
nikoalander1986 <niko.alander@hotmail.com>
2022-01-11 15:07:08 +0200

    ..

--- ensimmäisetHarjoitukset/omatProjektit/viikon2ToisetHarjoitukset/t5.js
+++ ensimmäisetHarjoitukset/omatProjektit/viikon2ToisetHarjoitukset/t5.js
@@ -1,6 +1,6 @@
 function laske() {
   let luku = parseInt(document.getElementById('lukema').value);
-  let lause = 
+  let lause =
 
   lause += luku + " x 1 = " + luku * 1;
   lause += luku + " x 2 = " + luku * 2;
@@ -12,7 +12,5 @@ function laske() {
   lause += luku + " x 8 = " + luku * 2;
   lause += luku + " x 9 = " + luku * 2;
   lause += luku + " x 10 = " + luku * 2;
-
-  document.getElementById('vastaus').textContent = lause;
 }
 //ei aukea tällä hetkellä tämä tehtävä
