c78f9e0b7908b4c5d291ff71c323d78acf9c6c0a
nikoalander1986 <niko.alander@hotmail.com>
2022-01-14 02:02:07 +0200

    ..

--- ensimmäisetHarjoitukset/omatProjektit/viikon3EnsimmäisetHarjoitukset/t2.js
+++ ensimmäisetHarjoitukset/omatProjektit/viikon3EnsimmäisetHarjoitukset/t2.js
@@ -1,4 +1,10 @@
 function muutaSalasana() {
   let heikko = document.getElementById('salasana').value;
-  
+  let uusi = "";
+
+  for (var i = 0; i < heikko.length; i++) {
+    uusi+=heikko+i+"Ö";
+    console.log(uusi);
+  }
 }
+//kesken
