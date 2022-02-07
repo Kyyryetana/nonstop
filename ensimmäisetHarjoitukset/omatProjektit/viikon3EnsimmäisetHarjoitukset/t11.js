d1eaa3edb70bb7da6ba708c2556034114d3793dc
nikoalander1986 <niko.alander@hotmail.com>
2022-02-08 00:03:24 +0200

    ...

--- ensimmäisetHarjoitukset/omatProjektit/viikon3EnsimmäisetHarjoitukset/t11.js
+++ ensimmäisetHarjoitukset/omatProjektit/viikon3EnsimmäisetHarjoitukset/t11.js
@@ -1,22 +1,31 @@
 function suorita() {
-  let pieniNumero = parseInt(document.getElementById('pienempiNumero').value);
-  let suuriNumero = parseInt(document.getElementById('suurempiNumero').value);
-  let parillinenJono = [];
-  let paritonJono = [];
+  let ekaNumero = parseInt(document.getElementById('pienempiNumero').value);
+  let tokaNumero = parseInt(document.getElementById('suurempiNumero').value);
+  let parillinenJono = '';
+  let paritonJono = '';
+  let parillinenSumma = 0;
+  let paritonSumma = 0;
+  let parillinenAlku;
+  let paritonAlku;
 
-  if (pieniNumero%2 == 0) {
-      for (let i = 0; i < suuriNumero; i++) {
-        parillinenJono.push();
-      }
+  if (ekaNumero%2 == 0) {
+    parillinenAlku = ekaNumero;
+  } else {
+    parillinenAlku = ekaNumero + 1;
+  } for (var i = parillinenAlku; i <= tokaNumero; i+=2) {
+      parillinenJono += i + " ";
+      parillinenSumma += i;
+  }
 
-      } else if (pieniNumero%2 != 0) {
-          for (let j = 1; i < suuriNumero; i+2) {
-            paritonJono.push();
-      }
+  if (ekaNumero%2 == 0) {
+    paritonAlku = ekaNumero + 1;
+  } else {
+    paritonAlku = ekaNumero;
+  } for (var j = paritonAlku; j <= tokaNumero; j+=2) {
+      paritonJono += j + " ";
+      paritonSumma += j;
+  }
+  document.getElementById('parillisetSummat').innerHTML = "Parilliset numerot ovat " + parillinenJono + "ja niiden summat on " + parillinenSumma;
 
-      } else {
-          alert("Yritä uudestaan");
-    }
-    console.log(parillinenJono);
+  document.getElementById('parittomatSummat').innerHTML = "Parittomat numerot ovat " + paritonJono + "ja niiden summat on " + paritonSumma;
 }
-//kesken
--- ensimmäisetHarjoitukset/omatProjektit/viikon3ToisetHarjoitukset/t4.js
+++ ensimmäisetHarjoitukset/omatProjektit/viikon3ToisetHarjoitukset/t4.js
@@ -1,13 +1,10 @@
 function arvoKortit() {
   let temp = [];
-  let pakka = [];
   for (var i = 0; i < 5; i++) {
     let maat = ['&#9828;', '&#9827;', '&#9826;', '&#9825;'][Math.floor(Math.random() * 4)]
-    temp.push(maat);
     let numerot = ['A','1','2','3','4','5','6','7','8','9','10','J','Q','K'][Math.floor(Math.random() * 14)]
-    temp.push(numerot)
+    temp.push(" " + maat + numerot);
   }
-  pakka.join(temp)
-  console.log(temp);
+  document.getElementById('vastaus').innerHTML = temp;
 }
-//kesken, ei ratkaistu vielä
+//Valmis toimii!
